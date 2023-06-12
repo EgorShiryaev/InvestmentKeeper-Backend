7b3f890029d39f8a2e675907b7aa6a25b71af467
EgorShiryaev <shiryaevvyatsu@gmail.com>
2023-06-12 23:06:13 +0300

    Добавлена версия api

--- src/app.ts
+++ src/app.ts
@@ -17,24 +17,33 @@ const App = ({ url, port, authApi, investApi }: Params) => {
         const app = express();
         const apiVersion = '/api/v1';
         app.use(express.json());
-        app.get('/isUser', authApi.isUser.handler);
-        app.post('/registration', authApi.registration.handler);
-        app.post('/login', authApi.login.handler);
-        const accountsPath = '/accounts';
+        app.get(`${apiVersion}/isUser`, authApi.isUser.handler);
+        app.post(`${apiVersion}/registration`, authApi.registration.handler);
+        app.post(`${apiVersion}/login`, authApi.login.handler);
+        const accountsPath = `${apiVersion}/accounts`;
         app.get(accountsPath, investApi.getAccounts.handler);
         app.post(accountsPath, investApi.createAccount.handler);
         app.put(accountsPath, investApi.updateAccount.handler);
-        app.post('/sales', investApi.createSale.handler);
-        app.post('/purchases', investApi.createPurchase.handler);
-        app.post('/refills', investApi.createRefill.handler);
-        app.post('/withdrawals', investApi.createWithdrawal.handler);
+        app.post(`${apiVersion}/sales`, investApi.createSale.handler);
+        app.post(`${apiVersion}/purchases`, investApi.createPurchase.handler);
+        app.post(`${apiVersion}/refills`, investApi.createRefill.handler);
+        app.post(
+          `${apiVersion}/withdrawals`,
+          investApi.createWithdrawal.handler,
+        );
         app.get(
-          '/searchInvestInstrument',
+          `${apiVersion}/searchInvestInstrument`,
           investApi.searchInvestInstrument.handler,
         );
-        app.get('/candles', investApi.getCandles.handler);
-        app.get('/instrumentPrice', investApi.getInstrumentPrice.handler);
-        app.get('/accountPrice', investApi.getAccountPrice.handler);
+        app.get(`${apiVersion}/candles`, investApi.getCandles.handler);
+        app.get(
+          `${apiVersion}/instrumentPrice`,
+          investApi.getInstrumentPrice.handler,
+        );
+        app.get(
+          `${apiVersion}/accountPrice`,
+          investApi.getAccountPrice.handler,
+        );
         const server = http.createServer(app);
         server.listen(port, url, () => {
           console.log(`Success start server ${url}:${port}`);
